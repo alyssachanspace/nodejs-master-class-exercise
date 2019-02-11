21fc6494dd854ed0e2526fadb8ad61b148f0de71
Alyssa Chan <alyssa@whiletrue.codes>
2019-02-11 12:50:03 +0800

    parse payloads

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // Dependencies
 const http = require('http')
 const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
 
 // The server should respond to all requests withh a string
 const server = http.createServer(function(req, res){
@@ -25,11 +26,21 @@ const server = http.createServer(function(req, res){
   // Get the headers as an object
   const headers = req.headers
 
-  // Send thhe respond
-  res.end('Hello world\n')
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8')
+  let buffer = ''
+  req.on('data', function(data){
+    buffer += decoder.write(data)
+  })
+  req.on('end', function(){
+    buffer += decoder.end()
 
-  // Log the request path
-  console.log('Request received with these headers: ', headers)
+    // Send thhe respond
+    res.end('Hello world\n')
+
+    // Log the request path
+    console.log('Request received with these payload: ', buffer)
+  })
 })
 
 // Start thhe server, and have it listen on port 3000
