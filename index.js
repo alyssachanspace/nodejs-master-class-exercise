a380822b060002025ec06a57562c4f06409e07ec
Alyssa Chan <alyssa@whiletrue.codes>
2019-02-11 15:14:09 +0800

    add ping handler

--- index.js
+++ index.js
@@ -5,10 +5,10 @@
 // Dependencies
 const http = require('http')
 const https = require('https')
+const config = require('./config')
 const fs = require('fs')
 const url = require('url')
 const StringDecoder = require('string_decoder').StringDecoder
-const config = require('./config')
 
 // Instantiate the HTTP server
 const httpServer = http.createServer(function(req, res){
@@ -101,10 +101,9 @@ const unifiedServer = function(req,res){
 // Define the handlers
 let handlers = {}
 
-// Sample handler
-handlers.sample = function(data,callback){
-  // Callback a http status code, and a payload object
-  callback(406,{'name': 'sample handler'})
+// Ping handler
+handlers.ping = function(data,callback){
+  callback(200)
 }
 
 // Not found handler
@@ -114,5 +113,5 @@ handlers.notFound = function(data,callback){
 
 // Define a request router
 const router = {
-  'sample': handlers.sample
+  'ping': handlers.ping
 }
